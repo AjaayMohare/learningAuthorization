1bf83ffc424f1c6583798c701f6b5ba1019c609d
ajaymohare <ajayyynot31@gmail.com>
2026-08-25 01:04:15 +0530

    main server file

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const app = require("./src/app");
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
