9a1af146332abf24795b9fcb0e05da9eb8aa647f
ajaymohare <ajayyynot31@gmail.com>
2026-08-28 02:06:56 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,6 @@
 require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 const { Pool } = require("pg");
 
-// console.log("HOST:", process.env.DB_HOST);
-// console.log("PORT:", process.env.DB_PORT);
-// console.log("DATABASE:", process.env.DB_NAME);
-// console.log("USER:", process.env.DB_USER);
-// console.log("PASSWORD TYPE:", typeof process.env.DB_PASSWORD);
-
 const pool = new Pool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
